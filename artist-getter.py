d4ac25a1ac41b830a7a41983441479f7fcd7fdd1
NojusKondrotas <nojus.kondrotas23@gmail.com>
2025-03-22 13:06:31 +0200

    feat Search for Items caller

--- artist-getter.py
+++ artist-getter.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 import os
 import base64
-from requests import post
+from requests import post, get
 import json
 
 load_dotenv()
@@ -28,5 +28,24 @@ def get_token():
 def get_auth_header(token):
     return {"Authorization": "Bearer " + token}
 
+def search_for_items(token, q, type, market, limit, offset):
+    endpoint_url = "https://api.spotify.com/v1/search"
+    headers = get_auth_header(token)
+    query = f"?q={q}&type={type}&market={market}&limit={limit}&offset={offset}"
+
+    query_url = endpoint_url + query
+
+    result = get(query_url, headers=headers)
+    json_result = json.loads(result.content)
+    if len(json_result) == 0:
+        print("No more artists could be found. Try changing the parameters or executing a different API call.")
+        return None
+    
+    return json_result
+
 token = get_token()
-print(token)
+
+result = search_for_items(token, "1900-2025", "artist", "LT", 50, 0)
+
+next_query_url = result["artists"]["next"]
+query_results = result["artists"]["items"]
